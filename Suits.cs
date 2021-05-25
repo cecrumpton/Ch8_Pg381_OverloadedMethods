4498a31673ecc67504506e8d2d9c3079a4a3a762
Corey Crumpton <31524125+cecrumpton@users.noreply.github.com>
2021-05-25 15:26:19 -0500

    Add files via upload

---
+++ Suits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_Pg381_OverloadedMethods
+{
+    enum Suits
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts
+    }
+}
